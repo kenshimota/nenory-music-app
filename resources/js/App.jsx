3a8cf0658dce56ecc4a1ee5bdcea0b1b582e1aaa
Viktor <viktorkiba.366@gmail.com>
2024-06-13 18:31:41 -0400

    se isieron  botones

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -7,7 +7,40 @@ import Example from "./components/Example";
 const App = () => (
     <>
        <Example />
-       <Button />
+       <Button size = "small">
+        Botton
+        </Button>
+
+
+        <Button size = "small" variant ="dark">
+        Botton
+        </Button>
+
+        <Button size = "small" variant ="outlined">
+        Botton
+        </Button>
+
+
+        <Button size = "large" disabled onClick ={console.log}>
+        Botton
+        </Button>
+
+
+
+        <Button>
+        Botton
+        </Button>
+       <Button size = "large">
+        Botton
+        </Button>
+
+        <Button size = "large" variant ="dark">
+        Botton
+        </Button>
+
+        <Button size = "large" variant ="outlined">
+        Botton
+        </Button>
     </>
 );
 
