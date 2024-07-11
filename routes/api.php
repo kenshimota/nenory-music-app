7b1447d3b0322f5f4c4e4807a956f38878b97ceb
Sebastian Yung <sebastianyungh@gmail.com>
2024-07-11 17:39:31 -0400

    hice singUp

--- routes/api.php
+++ routes/api.php
@@ -10,10 +10,12 @@ use App\Http\Controllers\API\StatesController;
 use App\Http\Controllers\API\CitiesController;
 use App\Http\Controllers\API\SuppliersController;
 use App\Http\Controllers\API\PurchasesController;
+use App\Http\Controllers\API\ProductController;
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('signin', [AuthController::class, 'signIn']);
-
+    Route::post('signup', [AuthController::class, 'signUp']);
+    
     Route::middleware(["auth:api", "auth"])->group(function() {
         Route::get('me', [AuthController::class, 'user']);
         Route::delete('signout', [AuthController::class,'signOut']);
@@ -28,5 +30,6 @@ Route::group([ "middleware" => ["auth:api", "auth"] ], function(){
     Route::apiResource("users",UserController::class);
     Route::apiResource("suppliers", SuppliersController::class);
     Route::apiResource("purchases", PurchasesController::class);
+    Route::apiResource("products", ProductController::class);
 });
 
