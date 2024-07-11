7a52fbea2e38b07efd5cf26e8a06278ff72cc552
Erik Mota Galindo <kenshimota@gmail.com>
2024-07-11 01:06:48 -0300

    the last changed

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,7 +11,7 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate 
+php artisan migrate --force
 
 echo "Seed"
-php artisan db:seed --class=DatabaseSeeder
+php artisan db:seed --force --class=DatabaseSeeder
