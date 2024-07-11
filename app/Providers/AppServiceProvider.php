8d4a1998727ab0fba9e968ec06e324bf27018631
Erik Mota Galindo <kenshimota@gmail.com>
2024-07-11 12:17:54 -0300

    the last changed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,10 @@ class AppServiceProvider extends ServiceProvider {
         Passport::refreshTokensExpireIn(now()->addDays(30));
         Passport::personalAccessTokensExpireIn(now()->addMonths(6));
 
+        /*
         if (env('APP_ENV') == 'production') {
             $url->forceScheme('https');
         }
+        */
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,13 +1,12 @@
 <?php
 ini_set('memory_limit', '512M');
 
-echo "hola";
-
-/*
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+
+try { 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
@@ -19,4 +18,8 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
-*/
+
+} catch(\Exception $e) {
+    // Return a 500 error
+    response()->json(['error' => $e->getMessage()], 500);
+}
