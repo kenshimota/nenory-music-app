f33f425a59e3092993e43cce3f10b309a24348a3
yesica <morenoyesica605@gmail.com>
2024-07-13 18:21:36 -0400

    corregi el boton exitclose

--- resources/js/components/ButtonExit/index.jsx
+++ resources/js/components/ButtonExit/index.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import IconButton from "@mui/material/IconButton";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
-
 import { useAuth } from "../Auth";
 
+// hola
 function ButtonExit() {
     const { clearToken } = useAuth();
 
     return (
-        <IconButton aria-label="ExitToApp" onClick={clearToken}>
-            <ExitToAppIcon />
-        </IconButton>
+        <React.Fragment>
+            <div style={{ flexGrow: 1 }}></div>
+            <div className="ml-auto">
+                <IconButton aria-label="ExitToApp" onClick={clearToken}>
+                    <ExitToAppIcon />
+                </IconButton>
+            </div>
+        </React.Fragment>
     );
 }
 
