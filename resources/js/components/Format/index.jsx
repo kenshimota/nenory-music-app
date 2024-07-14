ebf07144e693a8829794f03e3da669dca2c148a0
yesica <morenoyesica605@gmail.com>
2024-07-14 15:10:35 -0400

    Jessica

--- resources/js/components/Format/index.jsx
+++ resources/js/components/Format/index.jsx
@@ -1,1 +1,19 @@
 import React from "react";
+import moment from "moment";
+import numeral from "numeral";
+
+// Aquí puedes utilizar las funciones y métodos de numeral.js
+
+export const Date = ({ value }) => {
+    const date = moment(value);
+    const DateFormat = date.format("DD/MM/YYYY");
+
+    return DateFormat;
+};
+export const Number = ({ value }) => {
+    const numeroCedula = value;
+    const NumberFormat = `V-${numeral(numeroCedula).format("0,0")}`;
+    return NumberFormat.split(",").join(".");
+};
+
+export default { Number, Date };
